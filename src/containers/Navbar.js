56e9c03b01d2dc9a8c67773104511426fffaf0a1
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-25 20:43:55 -0400

    style navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Logo from '../images/twatter-logo.png'
 
 class Navbar extends Component {
     render() {
         return (
         <nav className="navbar navbar-expand">
-            
             <div className="container-fluid">
-              <Link to="/" className="navbar-brand" />
-              <img src="" alt="Twatter Home" />
+                
+                <div className="navbar-header">
+                    <Link to="/" className="navbar-brand" />
+                    <img src={Logo} alt="Twatter Home" />
+                </div>
+
+                <ul className="nav navbar-nav ml-auto">
+                    <li>
+                        <Link to="/signup">Sign Up</Link>
+                    </li>
+                    <li>
+                        <Link to="/signin">Log In</Link>
+                    </li>
+                </ul>
+
             </div>
 
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/signin">Log In</Link>
-              </li>
-            </ul>
 
         </nav>);
     }
